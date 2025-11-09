f9a3b5495fca0e066e6aa4730766a6e3846f826b
Egor Kotov <kotov.egor@gmail.com>
2025-11-09 10:24:18 +0100

    add internal helper to get ram stats in gb with ps

--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,30 @@ regex_match <- function(text, pattern, i = NULL, ...) {
   }
   match
 }
+
+#' Get available RAM
+#' @keywords internal
+#' @return A `numeric` amount of available RAM in GB.
+#' @noRd
+.get_ram_gb <- function(type = NULL) {
+  mem_info <- ps::ps_system_memory()
+
+  # Convert all values to GB and round to 2 decimal places
+  mem_info_gb <- lapply(mem_info, function(x) {
+    if (is.numeric(x)) {
+      return(round(x / 1024^3, 2))
+    }
+    return(x) # Keep non-numeric values as-is (like $percent)
+  })
+
+  # If type is specified, return just that value
+  if (!is.null(type)) {
+    if (!type %in% names(mem_info_gb)) {
+      stop("Memory type '", type, "' not available on this system")
+    }
+    return(mem_info_gb[[type]])
+  }
+
+  # Otherwise return the whole list
+  return(mem_info_gb)
+}
